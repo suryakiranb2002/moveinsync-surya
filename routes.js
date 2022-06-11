3819447dee121ac893e616c91861def9f23fbf5e
Surya Kiran Bellamkonda <suryakiran.b2002@gmail.com>
2022-06-11 13:33:13 +0530

    basic setup

--- routes.js
+++ routes.js
@@ -4,46 +4,102 @@ const express = require('express');
 const router = express.Router();
 var MongoClient = require('mongodb').MongoClient;
     var myurl = "mongodb://localhost:27017/moveinsync";
+    MongoClient.connect(myurl, function(err, db) {
+        if (err) throw err;
+        var dbo = db.db("moveinsync");
 
-    router.get('/addcustomerrecord', (req, res, next)=>{
-         db.collection('customers').insertOne({ text: req.body.text }, function (err,info) {res.json(info.ops[0])})
-           
-    
+    router.post('/addcustomerrecord', (req, res, next)=>{
+         dbo.collection('customers').insertOne({ text: req.body.text }, 
+            function (err,info) { 
+                if (err) throw err; console.log(JSON.stringify(info));
+            })     
         res.send({'message': 'Record inserted'});
-    });
-    router.get('/addporecord', (req, res, next)=>{
-        db.collection('purchaseorders').insertOne({ text: req.body.text }, function (err,info) {res.json(info.ops[0])})
-          
-   
-       res.send({'message': 'Record inserted'});
-   });
-   router.get('/addshippingdetail', (req, res, next)=>{
-    db.collection('shippingdetails').insertOne({ text: req.body.text }, function (err,info) {res.json(info.ops[0])})
-      
+    })
 
+    router.post('/addporecord', (req, res, next)=>{
+        dbo.collection('purchaseorders').insertOne({ text: req.body.text },
+             function (err,info) { 
+                if (err) throw err; 
+                console.log(JSON.stringify(info));
+            })  
+          res.send({'message': 'Record inserted'});
+   })
+
+   router.post('/addshippingdetail', (req, res, next)=>{
+    dbo.collection('shippingdetails').insertOne({ text: req.body.text }, 
+        function (err,info) { 
+            if (err) throw err; console.log(JSON.stringify(info));
+        })
+      
    res.send({'message': 'Record inserted'});
-});
+})
 
     router.get('/getcustomer/:name', (req, res, next)=>{
-         db.collection('customers').find( { City: req.params.name } )
+         dbo.collection('customers').find( { City: req.params.name },
+            function (err,info) { 
+                if (err) throw err; console.log(JSON.stringify(info));
+            } )
     
         console.log(req.params.name)
     
         res.send({'message': 'Successful'});
     });
-    router.post('/getcustomerswithPO', (req, res, next)=>{
-        db.collection('purchaseorders').find().toArray(function (err, items) {res.send(items)})
-    
-        
-    
-        res.send({'message': 'Successful'});
-    });
-    router.post('/getcustomerswithSD', (req, res, next)=>{
-        db.collection('shippingdetails').find().toArray(function (err, items) {res.send(items)})
-    
+    router.get('/getcustomerswithPO', (req, res, next)=>{
+       
+        dbo.collection('customers').aggregate([
+            { $lookup:
+               {
+                 from: 'purchaseorders',
+                 localField: 'CustomerID',
+                 foreignField: 'CustomerID',
+                 as: 'purchaseorderdetails'
+            
+            }
+          }
+         ]).toArray(function(err, res) {
+         if (err) throw err;
+         console.log(JSON.stringify(res));
+         })
+    router.get('/getcustomerswithSD', (req, res, next)=>{
+      
+        dbo.collection('customers').aggregate([
+            { $lookup:
+               {
+                 from: 'purchaseorders',
+                 localField: 'CustomerID',
+                 foreignField: 'CustomerID',
+                 as: 'purchaseorderdetails'
+            
+            }
+          }
+         
+         ]).toArray(function(err, res) {
+         if (err) throw err;
+         console.log(JSON.stringify(res));
+         });
+         dbo.collection('purchaseorders'.aggregate([
+            { $lookup:
+                {
+                  from: 'shippingdetails',
+                  localField: 'PurchaseOrderID',
+                  foreignField: 'PurchaseOrderID',
+                  as: 'shipmentdetails'
+             
+             }
+           }
+
+        ])).toArray(function(err, res) {
+        if (err) throw err;
+        console.log(JSON.stringify(res));
+        });
         
     
         res.send({'message': "Successful"});
     });
 
 module.exports = router;
+db.close();
+});
+});
+  
+
